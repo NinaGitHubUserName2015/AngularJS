fc542fb23198e4d8f000628147fb351386a053f9
NinaGitHubUserName2015 <ninamonkey@126.com>
2016-07-25 10:28:44 +0800

    practise AngularJS

---
+++ Practise/js/helloAngular.js
@@ -0,0 +1,4 @@
+function HelloAngular($scope)   //控制器
+{
+	$scope.greeting = {text:'Hello'};    //Model
+}
---
+++ Practise/js/phone.js
@@ -0,0 +1,17 @@
+function PhoneListCtrl($scope)
+{
+	$scope.phones = [
+		{
+			"name":"Nexus S",
+			"snippet":"Fast just got faster with Nexus S."
+		},
+		{
+			"name":"Motorola XOOM with WiFi",
+			"snippet":"The next generation."
+		},
+		{
+			"name":"Motorola XOOM",
+			"snippet":"The next generation."
+		}
+	];
+}
